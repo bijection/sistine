22ad4df7aeb1b8d718702c0d3d6c343f87b166ca
Guillermo <gui@mit.edu>
2016-07-09 14:27:17 -0700

    shitty object detection

--- sistine.py
+++ sistine.py
@@ -14,9 +14,15 @@ def main():
         # display
         # cv2.imshow('frame', frame)
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2LAB)
-        frame[:,:,0] = 0
-        frame[:,:,1] = 0
-        frame[:,:,2] = 255.0 * (frame[:,:,2] > 100)
+        # frame[:,:,0] = 0
+        # frame[:,:,1] = 0
+        # frame[:,:,2] = 255.0 * (frame[:,:,2] > 100)
+
+        x,y = cv2.minMaxLoc(frame[:,:,2])[2]
+        frame = cv2.cvtColor(frame, cv2.COLOR_LAB2RGB)
+        frame = cv2.rectangle(frame, (x-100,y-100), (x+100,y+100), 255)
+
+
         # l, a, b = cv2.split(frame)
         # l = cv2.threshold(l, 100)
         # frame = cv2.cvtColor(frame, cv2.COLOR_LAB2RGB)
