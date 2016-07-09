c3fea2827e7cb29c160b238f54f063484b9fa718
Anish Athalye <me@anishathalye.com>
2016-07-09 15:44:15 -0700

    Add largest contour detection

--- sistine.py
+++ sistine.py
@@ -5,6 +5,7 @@ import numpy as np
 # upper = np.array([45, 59, 50], dtype = "uint8")
 
 def main():
+    cv2.ocl.setUseOpenCL(False)
     cap = cv2.VideoCapture(0)
 
     # detector = cv2.SimpleBlobDetector()
@@ -20,11 +21,22 @@ def main():
         frame = cv2.inRange(frame[:,:,2], 90, 110)
 
         # frame = cv2.rectangle(frame, (x-100,y-100), (x+100,y+100), 255)
+        _, cnts, _ = cv2.findContours(frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        cv2.imshow('frame', frame)
+        best, bestarea = None, 0
+        for c in cnts:
+            area = cv2.contourArea(c)
+            if area > bestarea:
+                bestarea = area
+                best = c
+        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+        if best is not None:
+            cv2.drawContours(frame, [best], -1, (0, 255, 0), 5)
 
-        if cv2.waitKey(1) & 0xff == ord('q'):
-            break
+            cv2.imshow('frame', frame)
+
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                break
 
     # release everything
     cap.release()
